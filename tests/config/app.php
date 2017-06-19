1eb147e0a9efd90b4a8bea913393195582aa70fe
Shashwat Mishra <shashwat9630@gmail.com>
2017-06-19 13:17:14 +0530

    Fixed travis (2)

--- tests/config/app.php
+++ tests/config/app.php
@@ -144,7 +144,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Secrethash\Dropmenu\DropmenuServiceProvider::class
+        Secrethash\Dropmenu\DropmenuServiceProvider::class,
     ],
 
     /*
